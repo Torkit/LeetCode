8c2fb2ae6fa3cb8fd0d49edc7c2a5c793574fb08
Torkit <Torkit@users.noreply.github.com>
2013-11-25 20:53:24 -0500

    bit operation: xor

--- Single Number.java
+++ Single Number.java
@@ -1,4 +1,3 @@
-
 /**
   * Problem: Given an array of integers, every element appears twice except for one. Find that single one.
   * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
@@ -15,7 +14,6 @@
   * If there's only one element appears once, then if let a[0]^a[1]^...a[n-1], the twice of elements will become 0,
   * this operation result is our answer.
   */
-  
   
 public class Solution {
     public int singleNumber(int[] A) {
